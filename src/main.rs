413ef49e69e49e9b10339166de996d1d3e3abc61
Roshinator <sevaliaroshan@gmail.com>
2021-11-09 19:05:21 -0800

    Added drawing window test

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::rect::Rect;
 use std::time::Duration;
 use std::thread;
 
 mod game_boy_hardware;
+
+const SCREEN_WIDTH:i32 = 160;
+const SCREEN_HEIGHT:i32 = 144;
 fn main()
 {
     // let x: game_boy_hardware::cpu::CPU;
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("SDL2 Test", 800, 600)
+    let window = video_subsystem.window("Game Boy Inator", 800, 600)
         .position_centered()
+        .resizable()
         .build()
         .unwrap();
 
-    let mut canvas = window.into_canvas().build().unwrap();
-
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    let mut canvas = window.into_canvas().accelerated().build().unwrap();
+    canvas.set_logical_size(SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32).unwrap();
+    canvas.set_integer_scale(true).unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop
     {
@@ -34,7 +39,17 @@ fn main()
                 _ => {}
             }
         }
+        canvas.set_draw_color(Color::RGB(255, 0, 0));
         canvas.clear();
+        for i in 0..SCREEN_WIDTH
+        {
+            for j in 0..SCREEN_HEIGHT
+            {
+                let color = ((i % 2 == j % 2) as u8) * 255;
+                canvas.set_draw_color(Color::RGB(color, color, color));
+                canvas.fill_rect(Rect::new(i,j,1,1)).unwrap();
+            }
+        }
         canvas.present();
         thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
