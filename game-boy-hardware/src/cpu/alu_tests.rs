113c038ebfea178ccd1f68624153ac59947d18e4
Roshinator <sevaliaroshan@gmail.com>
2022-02-28 09:06:30 -0800

    added two add tests

--- game-boy-hardware/src/cpu/alu_tests.rs
+++ game-boy-hardware/src/cpu/alu_tests.rs
@@ -16,9 +16,127 @@ mod alu_tests
     }
 
     #[test]
+    #[ignore = "INCS cant be performed, flags not modified"]
     fn test_inc_r16()
     {
         let mut cpu = Cpu::new();
+        todo!();
+    }
+
+    #[test]
+    #[ignore = "INCS cant be performed, flags not modified"]
+    fn test_inc_sp()
+    {
+        let mut cpu = Cpu::new();
+        todo!();
         //INCS cant be performed, flags not modified
+    }
+
+    #[test]
+    #[ignore = "INCS cant be performed, flags not modified"]
+    fn test_inc_r8()
+    {
+        let mut cpu = Cpu::new();
+        todo!();
+        //INCS cant be performed, flags not modified
+    }
+
+    #[test]
+    #[ignore = "INCS cant be performed, flags not modified"]
+    fn test_inc_r16a()
+    {
+        let mut cpu = Cpu::new();
+        todo!();
+        //INCS cant be performed, flags not modified
+    }
+
+    #[test]
+    #[ignore = "INCS cant be performed, flags not modified"]
+    fn test_dec_r8()
+    {
+        let mut cpu = Cpu::new();
+        todo!();
+        //INCS cant be performed, flags not modified
+    }
+
+    #[test]
+    #[ignore = "INCS cant be performed, flags not modified"]
+    fn test_dec_r16a()
+    {
+        let mut cpu = Cpu::new();
+        todo!();
+        //INCS cant be performed, flags not modified
+    }
+
+    #[test]
+    #[ignore = "INCS cant be performed, flags not modified"]
+    fn test_dec_r16()
+    {
+        let mut cpu = Cpu::new();
+        todo!();
+        //INCS cant be performed, flags not modified
+    }
+
+    #[test]
+    #[ignore = "INCS cant be performed, flags not modified"]
+    fn test_dec_sp()
+    {
+        let mut cpu = Cpu::new();
+        todo!();
+        //INCS cant be performed, flags not modified
+    }
+
+    #[test]
+    fn test_add_r8_r8()
+    {
+        let mut cpu = Cpu::new();
+        //Test add
+        cpu.regs[REG_A] = 0b00000111;
+        cpu.regs[REG_B] = 1;
+        cpu.add_r8_r8(REG_A, REG_B);
+        assert_eq!(cpu.regs[REG_A], 8);
+        //Test flag H (ZNHC0000)
+        cpu.regs[REG_A] = 1;
+        cpu.regs[REG_B] = 0b00001111;
+        cpu.add_r8_r8(REG_A, REG_B);
+        assert_eq!(cpu.regs[REG_A], 16);
+        assert_eq!(cpu.regs[REG_F], 0b00100000);
+        //Test flag Z
+        cpu.regs[REG_A] = 0;
+        cpu.regs[REG_B] = 0;
+        cpu.add_r8_r8(REG_A, REG_B);
+        assert_eq!(cpu.regs[REG_A], 0);
+        assert_eq!(cpu.regs[REG_F], 0b10000000);
+        //Test flag C
+        cpu.regs[REG_A] = 0b11111111;
+        cpu.regs[REG_B] = 2;
+        cpu.add_r8_r8(REG_A, REG_B);
+        assert_eq!(cpu.regs[REG_A], 1);
+        assert_eq!(cpu.regs[REG_F], 0b00110000); //Half carry also occurs
+    }
+
+    #[test]
+    fn test_add_r8_8()
+    {
+        let mut cpu = Cpu::new();
+        //Test add
+        cpu.regs[REG_A] = 0b00000111;
+        cpu.add_r8_8(REG_A, 1);
+        assert_eq!(cpu.regs[REG_A], 8);
+        //Test flag H (ZNHC0000)
+        cpu.regs[REG_A] = 1;
+        cpu.add_r8_8(REG_A, 0b00001111);
+        assert_eq!(cpu.regs[REG_A], 16);
+        assert_eq!(cpu.regs[REG_F], 0b00100000);
+        //Test flag Z
+        cpu.regs[REG_A] = 0;
+        cpu.add_r8_8(REG_A, 0);
+        assert_eq!(cpu.regs[REG_A], 0);
+        assert_eq!(cpu.regs[REG_F], 0b10000000);
+        //Test flag C
+        cpu.regs[REG_A] = 0b11111111;
+        cpu.add_r8_8(REG_A, 2);
+        assert_eq!(cpu.regs[REG_A], 1);
+        assert_eq!(cpu.regs[REG_F], 0b00110000); //Half carry also occurs
     }
 }
